ff4a297bac1066b1885c1ef41eaf609c7d6ba7fd
Fanirykeziah <99541592+Fanirykeziah@users.noreply.github.com>
2022-08-03 23:12:30 +0300

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Aws from './Component/Aws';
+
+function App() {
+  return (
+    <div className="App">
+
+      <Aws/>
+    </div>
+  );
+}
+
+export default App;
